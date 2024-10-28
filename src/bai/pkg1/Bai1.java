27cc2bf99fa3d31b3b05a635bf6001fe1754ffd7
NguyenHuuSang21 <154437789+NguyenHuuSang21@users.noreply.github.com>
2024-10-28 23:41:58 +0700

    hoanganh

---
+++ src/bai/pkg1/Bai1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bai.pkg1;
+
+/**
+ *
+ * @author Admin
+ */
+public class Bai1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String hoTen = "Ngo Hoang Anh";
+        String lop = "CMU-TPM 15";
+        String maSoSinhVien = "123456789";
+
+  
+        System.out.println("Ten sinh vien: " + hoTen);
+        System.out.println("Lop: " + lop);
+        System.out.println("Ma so sinh vien: " + maSoSinhVien);
+    }
+    
+}
